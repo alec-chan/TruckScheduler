6876ec3b52b0b9a7d2210db6be173b177266e313
Alec <alec@Alecs-MacBook-Pro.local>
2017-10-08 11:53:08 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TruckScheduler.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Routes.cs
@@ -0,0 +1,4 @@
+using Nancy;
+using System;
+
+public 
